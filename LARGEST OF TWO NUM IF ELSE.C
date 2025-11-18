f41d28c4dd4d841b8716ad6aca7fa013fcf9753b
yashucoderai <naiksyashu739@gmail.com>
2025-11-18 23:36:57 +0530

    Create LARGEST OF TWO NUM IF ELSE.C

---
+++ LARGEST OF TWO NUM IF ELSE.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    if (a > b) {
+        printf("%d is the largest\n", a);
+    } 
+    else 
+    {
+        printf("%d is the largest\n", b);
+    }
+    return 0;
+}
